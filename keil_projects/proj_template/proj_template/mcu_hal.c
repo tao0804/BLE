8b9eabd93cf637c41c108860f9894d79824924d3
LAPTOP-GERBEHQB\tao <1>
2023-09-20 16:45:37 +0800

    修改：二版温度贴adc功能正常

--- keil_projects/proj_template/proj_template/mcu_hal.c
+++ keil_projects/proj_template/proj_template/mcu_hal.c
@@ -1,91 +1,41 @@
 #include "mcu_hal.h"
-
-// MCU_ADC_TAB* adcTable;
-
-// void adc_init(void)
-// {
-// 	CLK_EnableModuleClock(ADC_MODULE);
-
-// 	//P10设置成ADC channel1 analog input
-// 	SYS->P1_MFP |= SYS_MFP_P10_ADC_CH1;
-// 	//P12设置成ADC channel2 analog input
-// 	SYS->P1_MFP |= SYS_MFP_P12_ADC_CH2;
-
-// 	// Enable channel 2
-// 	// ADC_Open(ADC, 0, 0, 0x01 << 1);	// P10脚2.5v只采一次,P12采100次求平均值
-// 	ADC_Open(ADC, 0, 0, 0x01 << 2);
-
-// 	//Select ADC input range.1 means 0.4V~2.4V ;0 means 0.4V~1.4V.
-// 	//0.4V~2.4V & 0.4V~1.4V both are theoretical value,the real range is determined by bandgap voltage.
-// 	ADC_SelInputRange(ADC_INPUTRANGE_HIGH);
-
-// 	// Power on ADC
-// 	ADC_POWER_ON(ADC);
-
-// 	// Enable ADC convert complete interrupt
-// 	ADC_EnableInt(ADC, ADC_ADIF_INT);
-// 	NVIC_EnableIRQ(ADC_IRQn);
-// }
-
-// void adc_main(void)
-// {
-// 	while((ADC) -> STATUS |= ADC_STATUS_ADIF_Msk)	// 语句有问题，()里不能判断ADC_STATUS[0]是0是1
-// 	{// 转换完一次序列后处理
-// 		是否需要计算平均值
-// 		算实际电压值
-// 		开启下一次转换
-// 	}
-// }
-
-// uint16 Get_Adc_Average(uint32 adcChannel,uint16 rollCount)
-// {
-// 	uint32 adcCodeSum = 0;
-// 	for(int32_t t=0; t < rollCount; t++)
-// 	{
-// 		ADC_Open(ADC, 0, 0, adcChannel);	// 0x01 << 2
-// 		ADC_START_CONV(ADC);	// 我不知道完成中断在那里用，还是说，转换完会自动进?
-// 		ADC_EnableInt(ADC, ADC_ADIF_INT);
-// 		(ADC) -> STATUS |= ADC_STATUS_ADIF_Msk;	//  ADIF: This bit can be cleared to 0 by software writing 1
-// 												// 通过软件写1给ADIF位清0是否表示退出ADC完成中断
-// 		adcCodeSum += (uint32_t)ADC_GET_CONVERSION_DATA(ADC, 0);
-// 	}
-// 	return adcCodeSum/rollCount;
-// } 
-
-// uint32 adc_getVoltage(uint8 idx)
-// {
-// 	return adcTable[idx].voltage;
-// }
-
-// void adc_isr(void)
-// {
-// 	;// 转换完成中断处理
-// 	// 读取adcCode赋值给adcTablee[idx].adcCode
-// 	adcTable[idx].adcCodeSum += adcTablee[idx].adcCode;
-// 	// rollCount++;
-// 	// 是否有剩余的idx未转换
-// 	// 有的话 adcNowConvIdx++;ADC_Open(ADC, 0, 0, adcTablee[adcNowConvIdx].adcChannel);
-// 	//ADC_START_CONV(ADC);
-// 	// clear int flag?
-
-// }
-
-
-
-
+#include "stack_svc_api.h"
 
 // 用于追踪已完成的ADC转换次数
-volatile uint8_t adcCount = 0;
+uint32_t adcAvgCnt;
+uint32_t adcCodeSum;
+float adcValue;
+
+#define MCU_VREF 3.3
+#define BIT_OF_ADC 0xfff
+
+// 初始化GPIO,P10,ldo_en上拉
+void gpio_init(void)
+{
+	SYS->P1_MFP &= ~(SYS_MFP_P10_Msk);
+	SYS->P1_MFP |= SYS_MFP_P10_GPIO;
+	GPIO_InitOutput(P1, BIT0, GPIO_HIGH_LEVEL);
+	GPIO_PullUp(P1, BIT0, GPIO_PULLUP_ENABLE);
+	GPIO_ENABLE_DIGITAL_PATH(P1, BIT0);
+//	GPIO_SetBits(P1, BIT0);
+	GPIO_ClearBits(P1, BIT0);
+}
 
 // 初始化ADC
 void adc_init(void)
 {
 	CLK_EnableModuleClock(ADC_MODULE);
 
-	//P10设置成ADC channel1 analog input
-	SYS->P1_MFP |= SYS_MFP_P10_ADC_CH1;
+	//P13设置成ADC channel3 analog input
+	SYS->P1_MFP &= ~(SYS_MFP_P13_Msk);
+	SYS->P1_MFP |= SYS_MFP_P13_ADC_CH3;
+	GPIO_DISABLE_DIGITAL_PATH(P1, BIT3);
 	//P12设置成ADC channel2 analog input
+	SYS->P1_MFP &= ~(SYS_MFP_P12_Msk);
 	SYS->P1_MFP |= SYS_MFP_P12_ADC_CH2;
+	GPIO_DISABLE_DIGITAL_PATH(P1, BIT2);
+
+	((interrupt_register_handler)SVC_interrupt_register)(ADC_IRQ, adc_isr);
 
 	// Enable channel 2
 	// ADC_Open(ADC, 0, 0, 0x01 << 1);	// P10脚2.5v只采一次,P12采100次求平均值
@@ -101,41 +51,31 @@ void adc_init(void)
 	// Enable ADC convert complete interrupt
 	ADC_EnableInt(ADC, ADC_ADIF_INT);
 	NVIC_EnableIRQ(ADC_IRQn);
+	
+	ADC_START_CONV(ADC);
 }
 
-// ADC完成中断服务程序
-void adc_isr(MCU_ADC_TAB* p_adcVal)
+// ADC中断服务程序
+void adc_isr(void)
 {
-	p_adcVal = NULL;	// 初始化p_adcVal
-	// 读取ADC结果并进行处理
-    *p_adcVal += ADC_GET_CONVERSION_DATA(ADC, 0);	// 是否可以用volatile  adcValue代替MCU_ADC_TAB* p_adcVal
-    
-    // 增加已完成的转换次数
-    adcCount++;
-    
-    if (adcCount < 100) 
-	{
-        // 如果未完成100次转换,启动下一次ADC转换
-        ADC_START_CONV(ADC);
-    } 
-	else 
-	{
-        // 如果已完成100次转换,禁用ADC模块
-		ADC_POWER_DOWN(ADC);
-    }
-}// 指针的话, p_adcVal的值能保留,不用while,不阻塞
+	uint16 adcFlag = ADC_GET_INT_FLAG(ADC, ADC_ADIF_INT);
+	ADC_CLR_INT_FLAG(ADC, adcFlag);
 
-int adc_main() 
- {
-    adc_init();
-    
-    // 启动第一次ADC转换
+	adcCodeSum += ADC_GET_CONVERSION_DATA(ADC, 0);
+
+	// 增加已完成的转换次数
+	adcAvgCnt++;
+
+	if (adcAvgCnt >= 100) 
+	{
+		adcValue = adcCodeSum / adcAvgCnt * MCU_VREF / BIT_OF_ADC;
+		adcCodeSum = 0;
+		adcAvgCnt = 0;
+	}
 	ADC_START_CONV(ADC);
-    
-    // 主循环
-    while (1) {
-        // 在中断中处理ADC值，主循环可以执行其他任务
-    }
-    
-    return 0;
+}
+
+float adc_getVoltage(void)
+{
+	return adcValue;
 }
